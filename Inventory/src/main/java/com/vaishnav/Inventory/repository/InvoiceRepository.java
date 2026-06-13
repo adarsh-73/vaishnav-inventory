fb0c683e3de4b34aae9f99bf5c6208cc03ea3bb1
adarsh-73 <adarshsingh7380@gmail.com>
2026-06-13 11:21:38 +0530

    fix app speed and backend connection

--- Inventory/src/main/java/com/vaishnav/Inventory/repository/InvoiceRepository.java
+++ Inventory/src/main/java/com/vaishnav/Inventory/repository/InvoiceRepository.java
@@ -1,10 +1,16 @@
 package com.vaishnav.Inventory.repository;
 
 import com.vaishnav.Inventory.entity.Invoice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 
+    @Override
+    @EntityGraph(attributePaths = {"customer", "invoiceItems", "invoiceItems.productInvoiceitem"})
+    List<Invoice> findAll();
+
+    @EntityGraph(attributePaths = {"customer", "invoiceItems", "invoiceItems.productInvoiceitem"})
     List<Invoice> findByInvoiceNumber(String invoiceNumber);
 }
